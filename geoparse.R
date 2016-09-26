675e0689382612d35fcb5ae65d81bfa0c4ed2c49
Maëlle Salmon <maelle.salmon@yahoo.se>
2016-09-26 18:21:51 +0200

    Update geoparse.R

--- geoparse.R
+++ geoparse.R
@@ -2,7 +2,6 @@ library("dplyr")
 library("readr")
 library("purrr")
 library("geoparser")
-library("tidyr")
 song_lyrics <- read_csv("./billboard_lyrics_1964-2015.csv")
 
 Encoding(song_lyrics$Lyrics) <- "latin1"  # (just to make sure)
