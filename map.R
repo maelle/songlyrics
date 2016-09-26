16ed95212e1679330bea073ff1b68e7635876a47
masalmon <maelle.salmon@yahoo.se>
2016-09-26 18:12:30 +0200

    adds map

--- map.R
+++ map.R
@@ -1,14 +1,15 @@
 library("dplyr")
 library("leaflet")
 library("tidyr")
+library("htmlwidgets")
 load("song_lyrics.RData")
 
 unnested_lyrics <- unnest(song_lyrics, .out)
-unnested_lyrics %>%
+m <- unnested_lyrics %>%
   group_by(name, longitude, latitude, type) %>%
-  summarize(n = n()) %>%
+  summarize(n = n(), song = toString(Song)) %>%
   group_by(name) %>%
-  mutate(popup = paste(name, type, n)) %>%
+  mutate(popup = paste(name, type, n, song, sep = ",")) %>%
   ungroup() %>%
   filter(! type %in% c("continent")) %>%
   leaflet() %>%
@@ -16,3 +17,5 @@ unnested_lyrics %>%
   addCircleMarkers(lng = ~longitude,
                    lat = ~latitude,
                    popup = ~popup)
+
+saveWidget(m, "map.html")
